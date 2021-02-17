52ab0c9652a4d5d9142f1436e28550cf189c978f
sharon <martinezsharonr@gmail.com>
2021-02-16 18:16:44 -0800

    added pseudocode in javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+/// when user starts quiz by s=clicking start button
+
+
+//thats when timer starts and questions are asked
+
+
+// when question is answered then another question is asked
+
+
+// if user picks wrong answer then 10 seconds is deducted from the timer
+
+
+//else if answer is correct than add 10 points
+
+
+// game ends when all questions are answered or when timer reaches 0
+
+
+// when game ends then user can save their initials and score
+
